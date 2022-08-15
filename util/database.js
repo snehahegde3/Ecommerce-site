b98e92b9288e0c1b241df3b4b8d2de5eca8ae9f1
Sneha Hegde <snehahegde33@gmail.com>
2022-08-15 21:12:10 +0530

    working on database

--- util/database.js
+++ util/database.js
@@ -1,1 +1,11 @@
 const mysql = require('mysql');
+
+//pool to be able to run multiple queries at the same time
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'ecommerce-site',
+  password: '12345',
+});
+
+module.exports = pool.promise();
